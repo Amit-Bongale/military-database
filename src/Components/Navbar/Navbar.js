73b037cec4565f6f77e39fdb34d4f7d3f8b46c6e
Amit-Bongale <amitbongale14@gmail.com>
2024-02-09 20:41:33 +0530

    color pallet added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Navbar(){
   return (
-    <div className='Nav-bar-container'>Navbar
-
-        <Link to='/insert'> <button>insert</button></Link>
-        <Link to='/update'> <button>update</button></Link>
+    <div className='Nav-bar-container'>
+      <h2>Military Dashboard</h2>
+      <div className='Navbar-buttons-container'>
+        <Link to='/insert'> <button className='nav-button'>insert</button></Link>
+        <Link to='/update'> <button className='nav-button'>update</button></Link>
+        <Link to='/update'> <button className='nav-button'>Delete</button></Link>
+      </div>
 
     </div>
   )
