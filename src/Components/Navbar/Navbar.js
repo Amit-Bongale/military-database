b7868cc16b93190ec9e6bd10cf3bdf8888fd9b05
Amit-Bongale <amitbongale14@gmail.com>
2024-02-09 21:10:59 +0530

    navigation section created

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 
+
 function Navbar(){
   return (
     <div className='Nav-bar-container'>
       <h2>Military Dashboard</h2>
       <div className='Navbar-buttons-container'>
-        <Link to='/insert'> <button className='nav-button'>insert</button></Link>
-        <Link to='/update'> <button className='nav-button'>update</button></Link>
+        <Link to='/details'> <button className='nav-button'>Details</button></Link>
+        <Link to='/insert'> <button className='nav-button'>Insert</button></Link>
+        <Link to='/update'> <button className='nav-button'>Update</button></Link>
         <Link to='/update'> <button className='nav-button'>Delete</button></Link>
       </div>
 
