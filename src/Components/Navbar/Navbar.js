21007b615c1f2c66c4ea5fc557a3911d72042ca2
Amit-Bongale <amitbongale14@gmail.com>
2024-02-11 18:14:30 +0530

    dropdown created for insert section

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,39 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { useState , useEffect} from 'react'
 
+import arrow from '../../Assets/Icons/downward-arrow (1).png'
 
 function Navbar(){
+
+  let [IsdropdownOpen , setIsdropdownOpen] = useState(false)
+  let [rotation , setrotation] = useState(0);
+
+  const handleopen = () => {
+    setIsdropdownOpen(!IsdropdownOpen);
+    setrotation(rotation+180);
+  }
+
   return (
     <div className='Nav-bar-container'>
-      <h2>Military Dashboard</h2>
+
       <div className='Navbar-buttons-container'>
+        <h2>Military Databse</h2>
         <Link to='/details'> <button className='nav-button'>Details</button></Link>
-        <Link to='/insert'> <button className='nav-button'>Insert</button></Link>
+     
+       
+        <button className='nav-button' onClick={() => { handleopen() }}>Insert 
+        <img id='arrow-btn' src={arrow} height={20}  style={{rotate:`${rotation}deg`}}></img>
+        </button>
+        { IsdropdownOpen && (
+          <div className='nav-subbutton-container' >
+            <Link to='/insert'> <button  className='nav-button'>Soldier</button> </Link>
+            <button  className='nav-button'>Department</button>
+            <button  className='nav-button'>Operation</button>
+          </div>
+        )}
+
+
         <Link to='/update'> <button className='nav-button'>Update</button></Link>
         <Link to='/update'> <button className='nav-button'>Delete</button></Link>
       </div>
