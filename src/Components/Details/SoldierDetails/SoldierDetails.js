ee0757468ba6dd017ab42113c6e2c5dc4be3af64
Amit-Bongale <amitbongale14@gmail.com>
2024-02-15 21:22:22 +0530

    details page created

--- src/Components/Details/SoldierDetails/SoldierDetails.js
+++ src/Components/Details/SoldierDetails/SoldierDetails.js
@@ -1,7 +1,79 @@
+import { useEffect , useState } from "react";
+
+import '../details.css'
 
 function SoldierDetails() {
+
+  let [Soldierdata , setsoldierdata] = useState([])
+
+  useEffect(() => {
+    try {
+      fetch('http://localhost:3000/soldierdetails', { method: "POST" })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Fetched data:', data);
+        setsoldierdata(data);
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+  },[])
+
   return (
-    <div>SoldierDetails</div>
+    <div> 
+
+        <div className='details-container'>
+          
+            <table className='details-table'>
+
+                <tr>
+                  <th className="details-table-header"> Soldier ID</th>
+                  <th className="details-table-header"> Name</th>
+                  <th className="details-table-header"> Date of birth</th>
+                  <th className="details-table-header"> Gender</th>
+                  <th className="details-table-header"> Address</th>
+                  <th className="details-table-header"> Salary</th>
+                </tr>
+                
+                {
+                    Soldierdata.map((elem) => (
+                     
+                      <tr className="deatils-table-row">
+
+                        <td className="details-table-data">
+                            {elem.soldier_id} 
+                        </td>
+                        <td className="details-table-data">
+                            {elem.name}
+                        </td>
+                        <td className="details-table-data">
+                            {elem.dob}
+                        </td>
+                        <td className="details-table-data">
+                            {elem.gender}
+                        </td>
+
+                        <td className="details-table-data">
+                            {elem.address}
+                        </td>
+                        <td className="details-table-data">
+                            {elem.Salary}
+                        </td>
+                        
+                      </tr>
+                     
+                    ))
+                }
+                
+            </table>
+        </div>
+
+    </div>
+
   )
 }
 
