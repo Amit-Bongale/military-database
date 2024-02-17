6c418a1e1680cde8e29e3bce505574d1daf9aa2e
Amit-Bongale <amitbongale14@gmail.com>
2024-02-17 16:26:40 +0530

    update soldier created

--- src/Components/Update/UpdateSoldier/Updatesoldiervalues.js
+++ src/Components/Update/UpdateSoldier/Updatesoldiervalues.js
@@ -2,16 +2,17 @@ import { useState , useEffect  } from "react";
 
 import '../Update.css'
 
+import closeicon from '../../../Assets/Icons/close.png'
+
 function Updatesoldiervalues({id ,  setupdate}) {
 
-    let [Soldierdata , setsoldierdata] = useState([])
-
-    let [soldier_id, setsoldier_id] = useState("");
+    let [soldier_id, setsoldierid] = useState("");
     let [name , setname] = useState("");
     let [dob , setdob] = useState("");
     let [gender , setgender] = useState("");
     let [address , setaddress] = useState("");
     let [salary , setsalary] = useState("");
+    // let [newid , setnewid] = useState("")
 
     useEffect(() => {
 
@@ -29,7 +30,14 @@ function Updatesoldiervalues({id ,  setupdate}) {
         .then((res) => res.json())
         .then((data) => {
           console.log('Fetched data:', data);
-          setsoldierdata(data);
+
+          setsoldierid(data[0].soldier_id);
+          setname(data[0].name);
+          setdob(data[0].dob);
+          setgender(data[0].gender);
+          setaddress(data[0].address);
+          setsalary(data[0].salary);
+
         })
         .catch(error => {
           console.log('Error fetching data:', error);
@@ -43,114 +51,117 @@ function Updatesoldiervalues({id ,  setupdate}) {
 
 
 
-    function send(){
+    function update(){
 
-        let data = {
-          'soldier_id' : soldier_id,
-          'name' : name,
-          'dob' : dob,
-          'gender':gender,
-          'address':address,
-          'salary':salary
-        }
-    
-    
-        try{
-          fetch(`http://localhost:3000/updatesoldier` ,
-          { method : "POST" , headers:{'Content-Type': 'application/json'} ,  body:JSON.stringify(data)})
-          .then((res) => res.json())
-          .catch((error) => console.log(error));
-          alert('Soldier added sucessfully');
-    
-        }
-        catch (error) {
-          console.log("error :", error)
-        }
-    
-        window.location.reload();
+      let data = {
+        // 'soldier_id' : soldier_id,
+        'soldier_id' : id,
+        'name' : name,
+        'dob' : dob,
+        'gender':gender,
+        'address':address,
+        'salary':salary,
+      }
+  
+      try{
+        fetch(`http://localhost:3000/updatesoldier` ,
+        { method : "POST" ,
+          headers:{'Content-Type': 'application/json'} , 
+          body:JSON.stringify(data)
+        })
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      }
+      catch (error) {
+        console.log("error:", error)
+      }
+
+      alert('updated sucessfully')
+
+      window.location.reload();
+
     }
 
   return (
-    <div className="card-wrapper" >
+    <div  >
+      
       <div className="card">
             
-        <button className="close-button" onClick={() => setupdate(false)}>close</button>
-          {
-              Soldierdata.map((elem) => (
-              <div key={elem.soldier_id} className="input-container">
-              <table className="input-table">
-                <tbody>
-      
-                  <tr>
-                    <td className="row">
-                      <label className="insert-label">Soldier_ID</label>
-                    </td>
-                    <td className="input-row">
-                      <input  className="insert-input" type="text" value={elem.soldier_id} required
-                      onChange={(val) => {setsoldier_id(val.target.value)}} />
-                    </td>
-                  </tr>
-      
-                  <tr>
-                    <td>
-                      <label className="insert-label">Name</label>
-                    </td>
-                    <td>
-                      <input className="insert-input" type="text" required value={elem.name}
-                      onChange={(val) => {setname(val.target.value)}}/>
-                    </td>
-                  </tr>
-      
-                  <tr>
-                    <td>
-                      <label className="insert-label">Date of Birth</label>
-                    </td>
-                    <td>
-                      <input className="insert-input" type="date" required value={elem.dob}
-                      onChange={(val) => {setdob(val.target.value)}}/>
-                    </td>
-                  </tr>
-      
-                  <tr>
-                    <td>
-                      <label className="insert-label">Gender</label>
-                    </td>
-                    <td> 
-                      <input className="insert-input" type="text" required value={elem.gender}
-                      onChange={(val) => {setgender(val.target.value)}}/>
-                    </td>
-                  </tr>
-      
-                  <tr>
-                    <td>
-                      <label className="insert-label">Permanent Address</label>
-                    </td>
-                    <td>
-                      <input className="insert-input" type="text" required value={elem.address}
-                      onChange={(val) => {setaddress(val.target.value)}}/>
-                    </td>
-                  </tr>
-      
-                  <tr>
-                    <td>
-                      <label className="insert-label">Salary</label>
-                    </td>
-                    <td>
-                      <input className="insert-input" type="text" required value={elem.salary}
-                      onChange={(val) => {setsalary(val.target.value)}}/>
-                    </td>
-                  </tr>
-      
-                  <button type="submit" className="insert-button" onClick={() => send }> Update </button>
-      
-                </tbody>
-      
-              </table>
-      
-            </div>
-          ))}
+        <button className="close-button" onClick={() => setupdate(false)}><img id="closeimage"
+        src={closeicon} alt="close" height={30} /></button>
+           
+          <div className="input-container">
+          <table className="input-table">
+            <tbody>
+              <tr>
+                <td className="row">
+                  <label className="insert-label">Soldier_ID : {soldier_id} </label>
+                </td>
+                <td className="input-row">
+                  {/* <input  className="insert-input" type="text" value={soldier_id} 
+                  onChange={(val) => {setsoldierid(val.target.value) }} ></input> */}
+                </td>
+              </tr>
 
+              <tr>
+                <td>
+                  <label className="insert-label">Name</label>
+                </td>
+                <td>
+                  <input className="insert-input" type="text" required value={name}
+                  onChange={(event) => {setname(event.target.value)}}/>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <label className="insert-label">Date of Birth</label>
+                </td>
+                <td>
+                  <input className="insert-input" type="date" required value={dob}
+                  onChange={(event) => { setdob(event.target.value) }}/>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <label className="insert-label">Gender</label>
+                </td>
+                <td> 
+                  <input className="insert-input" type="text" required value={gender}
+                  onChange={(event) => { setgender(event.target.value) }}/>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <label className="insert-label">Permanent Address</label>
+                </td>
+                <td>
+                  <input className="insert-input" type="text" required value={address}
+                  onChange={(event) => {setaddress(event.target.value)}}/>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <label className="insert-label">Salary</label>
+                </td>
+                <td>
+                  <input className="insert-input" type="text" required value={salary}
+                  onChange={(event) => { setsalary(event.target.value)}}/>
+                </td>
+              </tr>
+
+              <button type="submit" className="insert-button" onClick={() => update() }> Update </button>
+
+            </tbody>
+          </table>
+
+        </div>
       </div>
+
+      <div className="card-wrapper"></div>
     </div>
   )
 }
